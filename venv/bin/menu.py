0e855fbb00de3792c45a2dd6f5a34ab05ca8aaac
Renato Postigo <renato.postigo@ucsp.edu.pe>
2018-11-24 04:00:09 -0500

    estupido git merge

--- venv/bin/menu.py
+++ venv/bin/menu.py
@@ -6,13 +6,18 @@ import sys
 import vacas
 from vacas import Vaca
 import fichas
+import login
+
+user_list = list() #usuarios
+test_user = Usuario(['Renato Postigo','1234','renato@gg.com','mi super fundo ammigo','0000'])
+user_list.append(test_user)
 
 vaca_list = list() #vacas
 test_vaca = Vaca(['V724502','Vaca','Holstein','Hembra','09/09/2016','V721302','V721314'])
 test_vaca1 = Vaca(['V722502','Vaca','Holstein','Hembra','09/09/1940','V721702','V121314'])
 test_vaca2 = Vaca(['V731502','Toro','Holstein','Hembra','07/12/1978','V721702','V121314'])
 test_vaca3 = Vaca(['V789718','Toro','Holstein','Macho','01/10/1913','V721749','V121589'])
-test_vaca4 = Vaca(['V145574','Vaca','Holstein','Macho','02/07/1950','V721705','V121485'])
+test_vaca4 = Vaca(['V145574','Vaca' ,'Holstein','Macho','02/07/1950','V721705','V121485'])
 
 vaca_list.append(test_vaca)
 vaca_list.append(test_vaca1)
@@ -31,26 +36,34 @@ def buscar_vaca_retvaca(id):
         if (vaca.nro_arete == id):
             return vaca
 
+def get_machos():
+    machos = 0
+    for vaca in vaca_list:
+        if (vaca.sexo == 'Macho'):
+            machos += 1
+
+    return str(machos)
+
+def get_hembras():
+    hembras = 0
+    for vaca in vaca_list:
+        if (vaca.sexo == 'Hembra'):
+            hembras += 1
+
+    return str(hembras)
+
 def mod_pesos(id_vaca, le_list):
     vaquita = buscar_vaca(id_vaca)
     #vacaa = vaca_list[vaquita]
     vaca_list[vaquita].modify_weight(le_list)
     print ('vaquita peso destete: ' + vaca_list[vaquita].ficha_peso.destete)
 
-def get_machos():
-    machos = 0
-    for vaca in vaca_list:
-        if vaca.sexo == 'Macho':
-            machos += 1
+def mod_sanitaria(id_vaca, le_list):
+    vaquita = buscar_vaca(id_vaca)
+    vaca_list[vaquita].modify_sanitaria(le_list)
+    fichas.sanitaria_list.append(vaca_list[vaquita].ficha_sanitaria)
+    print ('Ficha Sanitaria ' + vaca_list[vaquita].ficha_sanitaria.fecha)
 
-    return machos
-
-def get_hembras():
-    hembras = 0
-    for vaca in vaca_list:
-        if vaca.sexo == 'Hembra':
-            hembras += 1
-    return hembras
 
 def create_vaca(l1, root): #root is tk()
     la_vaca = Vaca(l1)
@@ -150,14 +163,14 @@ def add_peso(root):
     faenado = Label(add_peso, text="Faenado: ")
     faenado.grid(column=0, row=4)
 
-    faenado_input = Entry(add_peso, show="*")
+    faenado_input = Entry(add_peso)
     faenado_input.grid(column=1, row=4)
 
     btn_regresar = Button(add_peso, text="Regresar", command=lambda: create_addmenu(root))
     btn_regresar.grid(column=0, row=6)
 
-    btn_crear = Button(add_peso, text='Agregar Peso', command=lambda: mod_pesos(nacimiento_input.get(), [destete_input.get(), anho_input.get(),
-                                                                                                         dos_input.get(), faenado_input.get()]))
+    btn_crear = Button(add_peso, text='Agregar Peso', command=lambda: mod_pesos(nacimiento_input.get(),
+    [destete_input.get(), anho_input.get(),dos_input.get(), faenado_input.get()]))
     btn_crear.grid(column=1,row=6)
 
     raise_frame(add_peso)
@@ -221,13 +234,13 @@ def add_sanitaria(root):
     observaciones_input = Entry(add_sanitaria)
     observaciones_input.grid(column=1, row=8)
 
-    # btn_confirm = Button(add_sanitaria, text="Confirmar", command=lambda: create_user(
-    #    [arete_input.get(), fecha_input.get(), signos_input.get(), peso_input.get(),
-    #     temp_input.get(), aretema_input.get()], root))
-    # btn_confirm.grid(column=1, row=7)
+    btn_crear = Button(add_sanitaria, text='Agregar Información Sanitaria', command=lambda: mod_sanitaria(arete_input.get(),
+    [fecha_input.get(), peso_input.get(), signos_input.get(), temp_input.get(),
+    frecuencia_input.get(),tratamiento_input.get(),diagnostico_input.get(),observaciones_input.get()]))
+    btn_crear.grid(column=0,row=9)
 
     btn_regresar = Button(add_sanitaria, text="Regresar", command=lambda: create_menu(root))
-    btn_regresar.grid(column=0, row=9)
+    btn_regresar.grid(column=1, row=9)
 
     raise_frame(add_sanitaria)
 
@@ -253,28 +266,28 @@ def see_inv(root):
     my_seeinvmenu.grid(column=0, row=0, sticky="nsew")
     root.title("Inventario del Ganado")
 
-    hemc= Label( my_seeinvmenu, text="Número de Crías Hembras: ")
+    hemc= Label( my_seeinvmenu, text="Número de Hembras: ")
     hemc.grid(column=0, row=0)
 
-    hemc_num = Label( my_seeinvmenu,text="No se puede saber porque no" )
+    hemc_num = Label( my_seeinvmenu,text=get_hembras())
     hemc_num.grid(column=1, row=0)
 
-    macc = Label(my_seeinvmenu, text="Número de Crías Machos: ")
+    macc = Label(my_seeinvmenu, text="Número de Machos: ")
     macc.grid(column=0, row=1)
 
-    macc_num = Label(my_seeinvmenu, text="No se puede saber porque no")
+    macc_num = Label(my_seeinvmenu, text=get_machos())
     macc_num.grid(column=1, row=1)
 
-    hem = Label(my_seeinvmenu, text="Número de Hembras: ")
+    hem = Label(my_seeinvmenu, text="Número de Crías Hembras: ")
     hem.grid(column=0, row=2)
 
-    hem_num = Label(my_seeinvmenu, text=get_hembras())
+    hem_num = Label(my_seeinvmenu, text="No hay forma de saber por nuestra incompetencia")
     hem_num.grid(column=1, row=2)
 
-    mac = Label(my_seeinvmenu, text="Número de Machos: ")
+    mac = Label(my_seeinvmenu, text="Número de Crías Machos: ")
     mac.grid(column=0, row=3)
 
-    mac_num = Label(my_seeinvmenu, text=get_machos())
+    mac_num = Label(my_seeinvmenu, text="No hay forma de saber por nuestra incompetencia")
     mac_num.grid(column=1, row=3)
 
     btn_regresar = Button(my_seeinvmenu, text="Regresar", command=lambda: create_seemenu(root))
@@ -337,80 +350,180 @@ def see_rep(root):
     btn_regresar = Button(my_seeinvmenu, text="Regresar", command=lambda: create_seemenu(root))
     btn_regresar.grid(column=0, row=1)
 
-def see_san(root):
+def see_sanitario(id, root):
     my_seeinvmenu = Frame(root)
     my_seeinvmenu.grid(column=0, row=0, sticky="nsew")
     root.title("Reporte Sanitario del Ganado")
 
-    c1 = Label(my_seeinvmenu, text="Número de Arete")
-    c1.grid(column=0, row=0)
+    my_vaca = buscar_vaca_retvaca(id)
 
-    c2 = Label(my_seeinvmenu, text="Fecha")
-    c2.grid(column=1, row=0)
+    i=0
+    j=1
+    for Sanitaria in fichas.sanitaria_list:
 
-    c3 = Label(my_seeinvmenu, text="Signos Clínicos")
-    c3.grid(column=2, row=0)
+        if id == Sanitaria.nro_arete:
+            c0 = Label(my_seeinvmenu, text="Ficha "+str(j))
+            c0.grid(column=0, row=i)
 
-    c4 = Label(my_seeinvmenu, text="Peso / Condición Corporal")
-    c4.grid(column=3, row=0)
-
-    c5 = Label(my_seeinvmenu, text="Temperatura")
-    c5.grid(column=4, row=0)
-
-    c6 = Label(my_seeinvmenu, text="Frecuencia Cardiáca")
-    c6.grid(column=5, row=0)
-
-    c7 = Label(my_seeinvmenu, text="Tratamiento")
-    c7.grid(column=6, row=0)
-
-    c8 = Label(my_seeinvmenu, text="Diagnóstico")
-    c8.grid(column=7, row=0)
-
-    c9 = Label(my_seeinvmenu, text="Observaciones")
-    c9.grid(column=8, row=0)
+            i = i+1
+            c2 = Label(my_seeinvmenu, text="Fecha")
+            c2.grid(column=0, row=i)
+            c12 = Label(my_seeinvmenu, text=Sanitaria.fecha)
+            c12.grid(column=1, row=i)
+            i = i+1
+            c3 = Label(my_seeinvmenu, text="Signos Clínicos")
+            c3.grid(column=0, row=i)
+            c13 = Label(my_seeinvmenu, text=Sanitaria.signos)
+            c13.grid(column=1, row=i)
+            i = i+1
+            c4 = Label(my_seeinvmenu, text="Peso / Condición Corporal")
+            c4.grid(column=0, row=i)
+            c14 = Label(my_seeinvmenu, text=Sanitaria.peso)
+            c14.grid(column=1, row=i)
+            i = i + 1
+            c5 = Label(my_seeinvmenu, text="Temperatura")
+            c5.grid(column=0, row=i)
+            c15 = Label(my_seeinvmenu, text=Sanitaria.temperatura)
+            c15.grid(column=1, row=i)
+            i = i + 1
+            c6 = Label(my_seeinvmenu, text="Frecuencia Cardiáca")
+            c6.grid(column=0, row=i)
+            c16 = Label(my_seeinvmenu, text=Sanitaria.temperatura)
+            c16.grid(column=1, row=i)
+            i = i + 1
+            c7 = Label(my_seeinvmenu, text="Tratamiento")
+            c7.grid(column=0, row=i)
+            c17 = Label(my_seeinvmenu, text=Sanitaria.tratamiento)
+            c17.grid(column=1, row=i)
+            i = i + 1
+            c8 = Label(my_seeinvmenu, text="Diagnóstico")
+            c8.grid(column=0, row=i)
+            c18 = Label(my_seeinvmenu, text=Sanitaria.diagnostico)
+            c18.grid(column=1, row=i)
+            i = i + 1
+            c9 = Label(my_seeinvmenu, text="Observaciones")
+            c9.grid(column=0, row=i)
+            c19 = Label(my_seeinvmenu, text=Sanitaria.observaciones)
+            c19.grid(column=1, row=i)
+            i = i+1
+            j = j+1
 
     btn_regresar = Button(my_seeinvmenu, text="Regresar", command=lambda: create_seemenu(root))
-    btn_regresar.grid(column=0, row=1)
+    btn_regresar.grid(column=0, row=i)
+
+def see_san(root):
+    my_seeinvmenu = Frame(root)
+    my_seeinvmenu.grid(column=0, row=0, sticky="nsew")
+    user = Label(my_seeinvmenu, text="Usuario: ", relief=RAISED)
+
+    user.grid(column=0, row=0)
+
+    user_input = Entry(my_seeinvmenu)
+    user_input.grid(column=1, row=0)
+    login_button = Button(my_seeinvmenu, text="Ingresar", command=lambda: see_sanitario(user_input.get(), root))
+    # login_button.pack()
+    login_button.grid(column=0, row=2)
+
+def see_ficha(id_vaca,root):
+    my_seeficha = Frame(root)
+    my_seeficha.grid(column=0, row=0, sticky="nsew")
+    vaquita = buscar_vaca_retvaca(id_vaca)
+    arete = Label(my_seeficha, text="Número de arete: ")
+    arete.grid(column=0, row=0)
+
+    arete1 = Label(my_seeficha, text=vaquita.nro_arete)
+    arete1.grid(column=1, row=0)
+
+    especie = Label(my_seeficha, text="Especie: ")
+    especie.grid(column=0, row=1)
+
+    especie1 = Label(my_seeficha, text=vaquita.especie)
+    especie1.grid(column=1, row=1)
+
+    raza = Label(my_seeficha, text="Raza: ")
+    raza.grid(column=0, row=2)
+
+    raza1 = Label(my_seeficha, text=vaquita.raza)
+    raza1.grid(column=1, row=2)
+
+    sexo = Label(my_seeficha, text="Sexo: ")
+    sexo.grid(column=0, row=3)
+
+    sexo1 = Label(my_seeficha, text=vaquita.sexo)
+    sexo1.grid(column=1, row=3)
+
+    fecha = Label(my_seeficha, text="Fecha de nacimiento: ")
+    fecha.grid(column=0, row=4)
+
+    fecha1 = Label(my_seeficha, text=vaquita.fecha_ncto)
+    fecha1.grid(column=1, row=4)
+
+    aretema = Label(my_seeficha, text="Nro de arete de la madre: ")
+    aretema.grid(column=0, row=5)
+
+    aretema1 = Label(my_seeficha, text=vaquita.nro_arete_madre)
+    aretema1.grid(column=1, row=5)
+
+    aretepa = Label(my_seeficha, text="Nro de arete del padre: ")
+    aretepa.grid(column=0, row=6)
+
+    aretepa_input = Label(my_seeficha, text=vaquita.nro_arete_padre)
+    aretepa_input.grid(column=1, row=6)
+
+    btn_regresar = Button(my_seeficha, text="Regresar", command=lambda: create_seemenu(root))
+    btn_regresar.grid(column=0, row=7)
+
+def see_peso(id_vaca,root):
+    my_seeficha2 = Frame(root)
+    my_seeficha2.grid(column=0, row=0, sticky="nsew")
+    vaquita = buscar_vaca_retvaca(id_vaca)
+    arete = Label(my_seeficha2, text="Destete: ")
+    arete.grid(column=0, row=0)
+
+    arete1 = Label(my_seeficha2, text=vaquita.ficha_peso.destete)
+    arete1.grid(column=1, row=0)
+
+    especie = Label(my_seeficha2, text="Año: ")
+    especie.grid(column=0, row=1)
+
+    especie1 = Label(my_seeficha2, text=vaquita.ficha_peso.anho)
+    especie1.grid(column=1, row=1)
+
+    raza = Label(my_seeficha2, text="Dos Años: ")
+    raza.grid(column=0, row=2)
+
+    raza1 = Label(my_seeficha2, text=vaquita.ficha_peso.two_years)
+    raza1.grid(column=1, row=2)
+
+    sexo = Label(my_seeficha2, text="Faenado: ")
+    sexo.grid(column=0, row=3)
+
+    sexo1 = Label(my_seeficha2, text=vaquita.ficha_peso.faenado)
+    sexo1.grid(column=1, row=3)
+
+    btn_regresar = Button(my_seeficha2, text="Regresar", command=lambda: create_seemenu(root))
+    btn_regresar.grid(column=0, row=4)
+
 
 def see_inf(root):
     my_seeinfmenu = Frame(root)
     my_seeinfmenu.grid(column=0, row=0, sticky="nsew")
     root.title("Fichas Informativas del Ganado")
 
-    num= Label( my_seeinfmenu, text="Número de Arete")
-    num.grid(column=0, row=0)
+    arete = Label(my_seeinfmenu, text="Número de arete: ")
+    arete.grid(column=0, row=0)
 
-    ficha_num = Label( my_seeinfmenu,text="Ficha del Bovino" )
-    ficha_num.grid(column=1, row=0)
+    arete_input = Entry(my_seeinfmenu)
+    arete_input.grid(column=1, row=0)
+
+    btn_ficha = Button(my_seeinfmenu, text="Ficha del Bovino", command=lambda: see_ficha(arete_input.get(),root))
+    btn_ficha.grid(column=0, row=1)
+
+    btn_ficha1 = Button(my_seeinfmenu, text="Peso del Bovino", command=lambda: see_peso(arete_input.get(), root))
+    btn_ficha1.grid(column=1, row=1)
 
     btn_regresar = Button(my_seeinfmenu, text="Regresar", command=lambda: create_seemenu(root))
-    btn_regresar.grid(column=0, row=1)
-
-def see_cos(root):
-    my_seecosmenu = Frame(root)
-    my_seecosmenu.grid(column=0, row=0, sticky="nsew")
-    root.title("Registro de Costos")
-
-    c1 = Label(  my_seecosmenu, text="Cantidad")
-    c1.grid(column=0, row=0)
-
-    c2 = Label( my_seecosmenu,text="Descripción" )
-    c2.grid(column=1, row=0)
-
-    c3 = Label( my_seecosmenu,text="Fecha" )
-    c3.grid(column=2, row=0)
-
-    c4 = Label(my_seecosmenu, text="Costo Unitario")
-    c4.grid(column=3, row=0)
-
-    c5 = Label(my_seecosmenu, text="Costo Total")
-    c5.grid(column=4, row=0)
-
-    c6 = Label(my_seecosmenu, text="Tipo de Documento")
-    c6.grid(column=5, row=0)
-
-    btn_regresar = Button( my_seecosmenu, text="Regresar", command=lambda: create_seemenu(root))
-    btn_regresar.grid(column=0, row=1)
+    btn_regresar.grid(column=0, row=2)
 
 def see_venani(root):
     my_venmenu = Frame(root)
@@ -560,7 +673,7 @@ def exit(root) :
     tkinter.messagebox.showinfo("Saliendo", message)
     sys.exit()
 
-def create_menu(root, usuario = 0):
+def create_menu(root, usuario = user_list[0]):
 
     my_menu = Frame(root)
     my_menu.grid(column=0, row=0, sticky="nsew")
